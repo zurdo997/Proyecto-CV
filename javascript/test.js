4aa556c4948945dfd3b993994bbcc5b8920cbe86
Alan <alanfranco97@hotmail.com>
2023-01-21 17:25:13 -0300

    Modificaciones finales
    Todo el proyecto esta testeado, y le aplique mejoras en la version movil con media queries

--- javascript/test.js
+++ javascript/test.js
@@ -1,16 +1,26 @@
 function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark");
+  var element = document.body;
+  element.classList.toggle("dark");
 }
 
-document.querySelector('button').addEventListener('click', function() {
-    const icon = this.querySelector('i');
+document.querySelector('.nocturno').addEventListener('click', function () {
+  const icon = this.querySelector('i');
+
+  if (icon.classList.contains('fa-sun')) {
+    icon.classList.remove('fa-sun');
+    icon.classList.add('fa-moon');
+  } else {
+    icon.classList.remove('fa-moon');
+    icon.classList.add('fa-sun');
+  }
+});
+
+function domicilio() {
+  var x = document.getElementById("dom");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
   
-    if (icon.classList.contains('fa-sun')) {
-      icon.classList.remove('fa-sun');
-      icon.classList.add('fa-moon');
-    } else {
-      icon.classList.remove('fa-moon');
-      icon.classList.add('fa-sun');
-    }
-  });
